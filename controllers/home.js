4cd64ffe571b070d3f89d079df85bf52d970a517
saike <kishkoglot@gmail.com>
2016-12-17 11:51:03 +0000

    added all users on home

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,24 @@
+import User from '../models/user';
+
 export const index = async (req, res, next) => {
 
   console.log('Current user: ', req.user);
 
+  let all_users;
+
+  try {
+
+    all_users = await User.find({})
+
+  } catch (err) {
+
+    throw err;
+
+  }
+
   let context = {
-    user: req.user
+    user: req.user,
+    all_users: all_users || []
   };
 
   res.render('home', { locals: context } );
